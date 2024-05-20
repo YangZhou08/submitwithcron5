2406b54272d1662ab011bbd6e68baa05a896c36d
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-20 02:32:56 -0400

    correct

---
+++ bash_tasks/settingupgriffincluster.sh
@@ -0,0 +1,12 @@
+pwd 
+rm -rf GRIFFIN2 
+ls 
+cd ~ 
+git clone git@github.com:Infini-AI-Lab/GRIFFIN2.git 
+cd GRIFFIN2 
+source /data/home/beidic/.bashrc 
+source /data/home/beidic/anaconda3/etc/profile.d/conda.sh 
+conda env create -f griffin.yml 
+conda activate griffin 
+cd src 
+pip install lm_eval 
