a0db279e4e745f90059f55b53ebd70fd6dd510d2
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-31 03:08:06 -0700

    making

---
+++ bash_tasks/bringbackfile.sh
@@ -0,0 +1,5 @@
+mv /fsx-storygen/beidic/yang/log/log-2877096.out ../submitted 
+mv /fsx-storygen/beidic/yang/log/log-2877096.err ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-2711605.out ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-2711605.err ../submitted 
