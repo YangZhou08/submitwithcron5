3dc2ca0a3986d4a8691f6c3c2f6b7de552772138
keroro824 <bettychen824@gmail.com>
2024-05-31 10:40:02 +0000

    submit jobs

--- bash_tasks/bringbackfile.sh
+++
@@ -1,5 +0,0 @@
-mv /fsx-storygen/beidic/yang/log/log-2877154.out ../submitted 
-mv /fsx-storygen/beidic/yang/log/log-2877154.err ../submitted 
-
-mv /fsx-storygen/beidic/yang/log/log-2877153.out ../submitted 
-mv /fsx-storygen/beidic/yang/log/log-2877153.err ../submitted 
