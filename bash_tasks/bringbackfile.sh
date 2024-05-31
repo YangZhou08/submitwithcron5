80f7d40e40d8d3e482e6fe84fc519ec37b8616c3
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-05-31 08:48:53 -0700

    newjob

---
+++ bash_tasks/bringbackfile.sh
@@ -0,0 +1,11 @@
+mv /fsx-storygen/beidic/yang/log/log-2879432.out ../submitted 
+mv /fsx-storygen/beidic/yang/log/log-2879432.err ../submitted 
+
+mv /fsx-storygen/beidic/yang/log/log-2879431.out ../submitted 
+mv /fsx-storygen/beidic/yang/log/log-2879431.err ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-2877213.out ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-2877213.err ../submitted 
+
+# mv /fsx-storygen/beidic/yang/log/log-2877212.out ../submitted 
+# mv /fsx-storygen/beidic/yang/log/log-2877212.err ../submitted 
