9c29ed276b4bae12676173f7d38900727e42b69c
keroro824 <bettychen824@gmail.com>
2024-05-31 09:40:02 +0000

    submit jobs

--- submitted/launchnewjob3.sh
+++ submitted/launchnewjob3.sh
@@ -25,15 +25,16 @@
 ## SBATCH --array=0-11 # 12 jobs in total 
 
 source /data/home/beidic/.bashrc
-source /data/home/beidic/anaconda3/etc/profile.d/conda.sh 
-# source activate base
+# source /data/home/beidic/anaconda3/etc/profile.d/conda.sh 
+source /fsx-storygen/beidic/anaconda3/etc/profile.d/conda.sh
+source activate base
 # conda activate base 
 # mamba activate yangllm 
 # conda activate yangllm 
 conda activate hanshi 
 # cd /fsx-storygen/beidic/yang/transformersprofiling 
 cd /fsx-storygen/beidic/yang/GRIFFIN2 
-git checkout exp2 
+git checkout yangexp2 
 git pull 
 
 git pull 
