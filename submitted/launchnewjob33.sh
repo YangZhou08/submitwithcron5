e7fe7ca383d59570f50bdb5723639f4d6a783577
keroro824 <bettychen824@gmail.com>
2024-05-31 18:35:02 +0000

    submit jobs

--- submitted/launchnewjob33.sh
+++ submitted/launchnewjob33.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=1:00:00 
 
 ## partition name
-#SBATCH --partition=learnfair 
+#SBATCH --partition=storygen 
 #SBATCH --account=fairaws-storygen
 ## number of nodes
 #SBATCH --nodes=1
@@ -60,4 +60,4 @@ echo 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 | transformers-cli login
 huggingface-cli login --token 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 
 
 # /data/home/beidic/anaconda3/envs/griffin/bin/accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=True --tasks gsm8k --batch_size 1  
-/data/home/beidic/anaconda3/envs/hanshi/bin/accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=False --tasks gsm8k --batch_size 1 
+/data/home/beidic/anaconda3/envs/hanshi/bin/accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=False --tasks gsm8k --batch_size 1 --limit 0.5 
