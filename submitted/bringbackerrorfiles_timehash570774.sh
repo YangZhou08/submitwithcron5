400827d8e3050b97be7b978ae636e2e11a81be52
keroro824 <bettychen824@gmail.com>
2024-06-03 23:40:04 +0000

    submit jobs

---
+++ submitted/bringbackerrorfiles_timehash570774.sh
@@ -0,0 +1,33 @@
+mv /fsx-storygen/beidic/yang/log/log-2917176.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2917176.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2917175.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2917175.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2917174.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2917174.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2917173.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2917173.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2917172.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2917172.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916986.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916986.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916985.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916985.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916982.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916982.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916981.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916981.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916977.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916977.out ../submitted
+
+mv /fsx-storygen/beidic/yang/log/log-2916978.err ../submitted
+mv /fsx-storygen/beidic/yang/log/log-2916978.out ../submitted
+
