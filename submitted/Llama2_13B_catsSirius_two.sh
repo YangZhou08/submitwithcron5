83cce9e56b369c08295c4f66b95b0aadbc6e3688
keroro824 <bettychen824@gmail.com>
2024-06-05 03:15:03 +0000

    submit jobs

--- submitted/Llama2_13B_catsSirius_two.sh
+++ submitted/Llama2_13B_catsSirius_two.sh
@@ -63,4 +63,4 @@ huggingface-cli login --token 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
 
 # /data/home/beidic/anaconda3/envs/griffin/bin/accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=True --tasks gsm8k --batch_size 1  
 # accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,cats=True,check=True,kernel_size=16,spr=0.5,thr=0.1 --tasks gsm8k --batch_size 1 
-accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-hf,cats=True,check=True,kernel_size=16,spr=0.5,thr=0.1--tasks gsm8k --batch_size 1 
+accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-7b-hf,cats=True,check=True,kernel_size=16,spr=0.5,thr=0.1--tasks gsm8k --batch_size 1 
