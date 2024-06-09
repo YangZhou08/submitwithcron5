296210a37d76ca9a71d9962bff4ded8c07fc9c55
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-09 04:01:15 -0700

    newfiles

---
+++ bash_tasks/bringbackerrorfiles_timehash988949.sh
@@ -0,0 +1,6 @@
+cp /fsx-storygen/beidic/yang/log/log-2992513.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-2992513.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-2992512.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-2992512.out ../submitted
+
