bcf28d1fe5a907695b04f4ed59db0be792f44272
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-11 17:48:04 -0700

    logg

--- bash_tasks/bringbackerrorfiles.sh
+++ bash_tasks/bringbackerrorfiles.sh
@@ -1,5 +1,5 @@
 # cp /fsx-storygen/beidic/yang/log/log-2992513.err ../submitted 
 # cp /fsx-storygen/beidic/yang/log/log-2992513.out ../submitted 
 
-cp /fsx-storygen/beidic/yang/log/log-3039568.err ../submitted 
-cp /fsx-storygen/beidic/yang/log/log-3039568.out ../submitted 
+cp /fsx-storygen/beidic/yang/log/log-3041898.err ../submitted 
+cp /fsx-storygen/beidic/yang/log/log-3041898.out ../submitted 
