9b04adf1e3cf35352dd660e43a1f5b0aa4335b92
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-13 07:57:01 -0400

    ntetask

---
+++ bash_tasks/bringbackerrorfiles_timehash572440.sh
@@ -0,0 +1,15 @@
+cp /fsx-storygen/beidic/yang/log/log-3064942.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064942.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064941.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064941.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064940.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064940.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064939.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064939.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064938.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064938.out ../submitted
+
