a0eeadd59292915bcd735302742d8a4677d74bb7
keroro824 <bettychen824@gmail.com>
2024-06-13 09:50:03 +0000

    submit jobs

---
+++ submitted/bringbackerrorfiles_timehash559044.sh
@@ -0,0 +1,15 @@
+cp /fsx-storygen/beidic/yang/log/log-3064495.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064495.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064494.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064494.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064493.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064493.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064492.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064492.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3064491.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3064491.out ../submitted
+
