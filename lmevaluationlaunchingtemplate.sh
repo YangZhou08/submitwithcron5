0ba11417b2dd80a6864032b191fe764c5522f317
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-19 09:18:14 -0400

    MODE:L

--- lmevaluationlaunchingtemplate.sh
+++ lmevaluationlaunchingtemplate.sh
@@ -9,11 +9,11 @@ models=(
 
 # List of commands to run
 commands=(
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,cats=False,check=False --tasks gsm8k --batch_size 1 "
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,cats=True,check=False --tasks gsm8k --batch_size 1 "
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,cats=True,check=True,kernel_size=16,spr=0.5,thr=0.1 --tasks gsm8k --batch_size 1 "
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,griffin=True,check=False --tasks gsm8k --batch_size 1 "
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,griffin=True,check=True,kernel_size=16,spr=0.5,thr=0.1 --tasks gsm8k --batch_size 1 "
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=MODEL,cats=False,check=False --tasks gsm8k --batch_size 1 "
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=MODEL,cats=True,check=False --tasks gsm8k --batch_size 1 "
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=MODEL,cats=True,check=True,kernel_size=16,spr=0.5,thr=0.1 --tasks gsm8k --batch_size 1 "
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=MODEL,griffin=True,check=False --tasks gsm8k --batch_size 1 "
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --model xhf --model_args pretrained=MODEL,griffin=True,check=True,kernel_size=16,spr=0.5,thr=0.1 --tasks gsm8k --batch_size 1 "
 )
 
 # Iterate over the commands and submit a job for each
@@ -23,7 +23,8 @@ for cmd in "${commands[@]}"; do
         unique_id=$(date +%s%N | sha256sum | base64 | head -c 5)
         job_script=$(mktemp /fsx-storygen/beidic/yang/submitwithcron5/submitted/job-$unique_id-XXXXXX.sh)
 
-        fullcmd="$cmd --model $model"
+        # fullcmd="$cmd --model $model"
+        fullcmd="${cmd/MODEL/$model}"
         
         # Fill in the placeholder in the template with the actual command
         sed "s|COMMAND_PLACEHOLDER|$fullcmd|g" /fsx-storygen/beidic/yang/submitwithcron5/lm_eval_harness_temp.sh > "$job_script"
