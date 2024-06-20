c9f8e9a75142c760cb2aba6f0cdc943afa256aba
keroro824 <bettychen824@gmail.com>
2024-06-20 05:45:03 +0000

    submit jobs

--- submitted/codegenerationlaunchingtemplate.sh
+++ submitted/codegenerationlaunchingtemplate.sh
@@ -2,21 +2,21 @@
 
 models=(
     "meta-llama/Meta-Llama-3-8B-Instruct"
-    "meta-llama/Meta-Llama-3-8B"
-    "meta-llama/Llama-2-7b-chat-hf"
+    # "meta-llama/Meta-Llama-3-8B"
+    # "meta-llama/Llama-2-7b-chat-hf"
     "meta-llama/Llama-2-7b-hf"
 ) 
 
 # List of commands to run
 commands=(
-    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution" 
-    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --griffin --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution" 
-    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --cats --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution" 
-    "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --griffin --check --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution" 
-    "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --cats --check --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution" 
-    "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
-    "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --griffin --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
-    "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --cats --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --griffin --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --cats --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --griffin --check --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks humaneval --do_sample False --cats --check --n_samples 1 --batch_size 1 --max_length_generation 512 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --griffin --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
+    # "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --cats --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
     "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --griffin --check --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
     "accelerate launch --num_processes 8 main.py --tasks mbpp --do_sample False --cats --check --n_samples 1 --batch_size 1 --max_length_generation 2048 --enable_epatches --allow_code_execution"
 )
