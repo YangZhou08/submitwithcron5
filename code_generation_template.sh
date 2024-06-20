c96a204da0784bb9e93e2811bec77c57a1d1f3e9
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-06-20 01:41:59 -0400

    modify

--- code_generation_template.sh
+++ code_generation_template.sh
@@ -8,7 +8,7 @@
 ## filename for job standard error output (stderr)
 #SBATCH --error=/fsx-storygen/beidic/yang/log/log-%j.err
 
-#SBATCH --time=2:00:00 
+#SBATCH --time=3:00:00 
 
 ## partition name
 #SBATCH -q storygen 
