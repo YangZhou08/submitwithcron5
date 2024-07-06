3a025d22f49c80a7e5176eb2d5a200c8867531fa
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-06 16:46:47 -0400

    setupsstream

---
+++ bash_tasks/makingsurenewbranchplled.sh
@@ -0,0 +1,8 @@
+cd /fsx-storygen/beidic/yang/GRIFFIN2 
+git branch 
+git pull 
+git checkout yangexp2threee 
+git branch 
+git pull 
+# git branch --set-upstream-to=origin/<branch> yangexp2threee
+git branch --set-upstream-to=origin/yangexp2threee yangexp2threee 
