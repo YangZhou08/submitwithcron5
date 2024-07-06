c5996b85ad7ffcdb3285ae45e82fecf499502006
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-06 17:34:15 -0400

    newjobs

---
+++ bash_tasks/bringbackerrorfiles_timehash292574.sh
@@ -0,0 +1,9 @@
+cp /fsx-storygen/beidic/yang/log/log-3286846.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3286846.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3286840.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3286840.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3286810.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3286810.out ../submitted
+
