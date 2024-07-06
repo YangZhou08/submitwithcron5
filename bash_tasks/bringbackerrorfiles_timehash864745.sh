6e13dc3f4cf94b4f89dbc4d4cb943969b0dcac2f
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-06 08:43:31 -0400

    bringbacke

---
+++ bash_tasks/bringbackerrorfiles_timehash864745.sh
@@ -0,0 +1,168 @@
+cp /fsx-storygen/beidic/yang/log/log-3285771.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285771.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285770.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285770.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285769.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285769.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285768.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285768.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285767.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285767.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285766.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285766.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285765.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285765.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285764.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285764.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285763.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285763.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285762.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285762.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285761.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285761.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285760.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285760.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285759.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285759.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285758.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285758.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285757.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285757.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285756.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285756.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285755.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285755.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285754.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285754.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285753.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285753.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285809.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285809.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285808.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285808.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285807.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285807.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285806.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285806.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285805.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285805.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285804.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285804.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285803.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285803.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285802.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285802.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285801.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285801.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285800.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285800.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285799.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285799.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285798.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285798.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285797.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285797.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285796.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285796.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285795.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285795.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285794.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285794.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285793.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285793.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285792.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285792.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285791.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285791.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285790.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285790.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285789.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285789.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285788.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285788.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285787.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285787.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285786.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285786.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285750.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285750.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285751.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285751.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285752.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285752.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285740.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285740.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285741.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285741.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285742.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285742.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285743.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285743.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285744.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285744.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285745.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285745.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285746.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285746.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285747.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285747.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285748.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285748.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3285749.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3285749.out ../submitted
+
