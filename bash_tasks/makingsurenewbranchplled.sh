be18763def5324fdbfd02a19ba20aabb6062cf6f
keroro824 <bettychen824@gmail.com>
2024-07-06 20:50:04 +0000

    submit jobs

--- bash_tasks/makingsurenewbranchplled.sh
+++
@@ -1,7 +0,0 @@
-cd /fsx-storygen/beidic/yang/GRIFFIN2 
-git branch 
-git checkout yangexp2threee 
-git branch 
-git branch --set-upstream-to=origin/yangexp2threee yangexp2threee 
-git pull 
-# git branch --set-upstream-to=origin/<branch> yangexp2threee 
--- submitted/makingsurenewbranchplled.sh
+++ submitted/makingsurenewbranchplled.sh
@@ -1,6 +1,7 @@
 cd /fsx-storygen/beidic/yang/GRIFFIN2 
 git branch 
-git pull 
 git checkout yangexp2threee 
 git branch 
+git branch --set-upstream-to=origin/yangexp2threee yangexp2threee 
 git pull 
+# git branch --set-upstream-to=origin/<branch> yangexp2threee 
