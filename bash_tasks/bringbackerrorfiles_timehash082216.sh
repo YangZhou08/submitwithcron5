93348070dbaea1051b09da077d9519d8402143eb
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-06 22:15:30 -0400

    bringbackjobs

---
+++ bash_tasks/bringbackerrorfiles_timehash082216.sh
@@ -0,0 +1,9 @@
+cp /fsx-storygen/beidic/yang/log/log-3287229.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287229.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3287228.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287228.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
