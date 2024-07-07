903fdcf19bb455e635ce2b8db1bebb786fdfa122
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-06 21:15:33 -0400

    hring

---
+++ bash_tasks/bringbackerrorfiles_timehash365286.sh
@@ -0,0 +1,15 @@
+cp /fsx-storygen/beidic/yang/log/log-3287163.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287163.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3287162.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287162.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3287161.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287161.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3287090.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3287090.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
