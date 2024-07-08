57f1d56860c898b19dbdf9a40ed161ec06999dbf
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-08 15:22:24 -0400

    usingnewtasks

---
+++ bash_tasks/bringbackerrorfiles_timehash154326.sh
@@ -0,0 +1,63 @@
+cp /fsx-storygen/beidic/yang/log/log-3292160.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292160.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292159.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292159.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292158.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292158.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292157.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292157.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292156.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292156.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292155.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292155.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292154.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292154.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292153.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292153.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292140.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292140.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292141.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292141.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292142.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292142.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292143.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292143.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292144.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292144.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292145.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292145.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292145.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292145.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292144.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292144.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292143.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292143.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292142.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292142.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292141.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292141.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3292140.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3292140.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
