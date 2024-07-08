75a609e9366ae1c9ab5caf35020cf6b173a5c469
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-08 17:05:10 -0400

    bringbackre

---
+++ bash_tasks/bringbackerrorfiles_timehash459520.sh
@@ -0,0 +1,21 @@
+cp /fsx-storygen/beidic/yang/log/log-3293704.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293704.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3293703.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293703.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3293702.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293702.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3293701.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293701.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3293700.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293700.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3293699.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3293699.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
