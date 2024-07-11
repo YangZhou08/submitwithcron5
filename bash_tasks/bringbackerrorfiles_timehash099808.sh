3ffef0cd672fccfbcb5833c1942cbd8e22954a5e
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-11 01:45:19 -0400

    m

---
+++ bash_tasks/bringbackerrorfiles_timehash099808.sh
@@ -0,0 +1,63 @@
+cp /fsx-storygen/beidic/yang/log/log-3313351.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313351.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313350.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313350.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313349.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313349.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313348.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313348.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313345.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313345.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313343.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313343.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313338.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313338.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313337.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313337.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313339.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313339.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313340.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313340.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313341.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313341.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313342.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313342.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313344.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313344.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313346.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313346.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313347.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313347.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313332.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313332.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313333.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313333.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313334.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313334.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313335.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313335.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3313336.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3313336.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
