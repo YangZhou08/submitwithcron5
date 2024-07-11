5e5cee5fdee92178f07a4a582e291f7e8b348228
keroro824 <bettychen824@gmail.com>
2024-07-11 15:55:04 +0000

    submit jobs

--- submitted/commonsenselaunchingtemplate.sh
+++ submitted/commonsenselaunchingtemplate.sh
@@ -2,7 +2,7 @@
 
 models=(
     # "meta-llama/Meta-Llama-3-8B-Instruct"
-    # "meta-llama/Meta-Llama-3-8B"
+    "meta-llama/Meta-Llama-3-8B"
     # "meta-llama/Llama-2-7b-chat-hf"
     # "meta-llama/Llama-2-7b-hf"
     "meta-llama/Llama-2-13b-chat-hf"
