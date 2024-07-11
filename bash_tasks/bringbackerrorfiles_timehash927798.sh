d7f430764f7d3180df7419e7387c2d22ce60741d
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-11 11:32:09 -0400

    bringbackothers

---
+++ bash_tasks/bringbackerrorfiles_timehash927798.sh
@@ -0,0 +1,42 @@
+cp /fsx-storygen/beidic/yang/log/log-3315018.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315018.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315017.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315017.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315019.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315019.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315020.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315020.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315021.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315021.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315022.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315022.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315023.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315023.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315024.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315024.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315025.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315025.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315047.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315047.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315048.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315048.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315043.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315043.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315044.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315044.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
