d43c4b0f3158901c4fd7877697ec73087e2d87bd
keroro824 <bettychen824@gmail.com>
2024-07-11 16:10:03 +0000

    submit jobs

---
+++ submitted/bringbackerrorfiles_timehash163329.sh
@@ -0,0 +1,30 @@
+cp /fsx-storygen/beidic/yang/log/log-3315563.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315563.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315535.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315535.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315539.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315539.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315540.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315540.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315541.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315541.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315562.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315562.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315564.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315564.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315565.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315565.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3315566.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3315566.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
