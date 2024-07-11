d02a17cd97cd219e7e8afc91a60d2c632343a72f
keroro824 <bettychen824@gmail.com>
2024-07-11 05:45:03 +0000

    submit jobs

--- submitted/commonsenselaunchingtemplate.sh
+++ submitted/commonsenselaunchingtemplate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 models=(
-    "meta-llama/Meta-Llama-3-8B-Instruct"
+    # "meta-llama/Meta-Llama-3-8B-Instruct"
     "meta-llama/Meta-Llama-3-8B"
     # "meta-llama/Llama-2-7b-chat-hf"
     # "meta-llama/Llama-2-7b-hf"
-    "meta-llama/Llama-2-13b-chat-hf"
-    "meta-llama/Llama-2-13b-hf"
+    # "meta-llama/Llama-2-13b-chat-hf"
+    # "meta-llama/Llama-2-13b-hf"
 ) 
 
 # List of commands to run
@@ -18,9 +18,9 @@ commands=(
     # "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --griffin --tasks csqa,strategyqa,sports,date --check --kernel_size 16 --spr 0.3 --thr 0.1"
     "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --tasks aqua --shotfive"
     "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --cats --tasks aqua --shotfive"
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --cats --tasks aqua --check --kernel_size 16 --spr 0.5 --thr 0.1 --shotfive --patternstrict --widthtree 1" 
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --cats --tasks aqua --check --kernel_size 16 --spr 0.5 --thr 0.1 --shotfive --patternstrict --widthtree 1 --filteractiveenabled" 
     "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --griffin --tasks aqua --shotfive"
-    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --griffin --tasks aqua --check --kernel_size 16 --spr 0.5 --thr 0.1 --shotfive --widthtree 1" 
+    "accelerate launch --main_process_port 29510 --num_processes 8 --num_machines 1 main.py --griffin --tasks aqua --check --kernel_size 16 --spr 0.5 --thr 0.1 --shotfive --widthtree 1 --filteractiveenabled" 
 )
 
 # Iterate over the commands and submit a job for each
