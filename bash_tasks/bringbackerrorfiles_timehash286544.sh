e14422674dd61927c0ed8f1cf78d3b94fa2bc468
Yang Zhou <yangzhou@Yangs-MacBook-Pro-4.local>
2024-07-12 18:23:34 -0400

    300

---
+++ bash_tasks/bringbackerrorfiles_timehash286544.sh
@@ -0,0 +1,18 @@
+cp /fsx-storygen/beidic/yang/log/log-3328303.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3328303.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3328304.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3328304.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3328305.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3328305.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3328306.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3328306.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3328307.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3328307.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
