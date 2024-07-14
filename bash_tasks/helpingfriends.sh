4527ab28d8dc024df5ba95b27dddd5440259106a
keroro824 <bettychen824@gmail.com>
2024-07-14 20:20:03 +0000

    submit jobs

--- bash_tasks/helpingfriends.sh
+++
@@ -1,6 +0,0 @@
-cd /data/home/beidic/hanshi/Rhodonite-Archive/
-rm .git/gc.log
-git prune
-rm -f .git/index.lock
-git pull --rebase origin main
-git push origin main
--- submitted/helpingfriends.sh
+++ submitted/helpingfriends.sh
@@ -1,1 +1,6 @@
-bash /data/home/beidic/hanshi/Rhodonite-Archive/execute.sh 
+cd /data/home/beidic/hanshi/Rhodonite-Archive/
+rm .git/gc.log
+git prune
+rm -f .git/index.lock
+git pull --rebase origin main
+git push origin main
