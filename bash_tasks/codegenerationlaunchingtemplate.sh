cb85fe00384dd24536f374e47a18785a1f0aedc2
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-16 14:53:16 -0400

    addingtwo

--- bash_tasks/codegenerationlaunchingtemplate.sh
+++ bash_tasks/codegenerationlaunchingtemplate.sh
@@ -10,8 +10,8 @@ models=(
     "meta-llama/Meta-Llama-3-8B"
     "meta-llama/Llama-2-7b-chat-hf"
     "meta-llama/Llama-2-7b-hf"
-    # "meta-llama/Llama-2-13b-chat-hf"
-    # "meta-llama/Llama-2-13b-hf"
+    "meta-llama/Llama-2-13b-chat-hf"
+    "meta-llama/Llama-2-13b-hf"
 ) 
 
 # List of commands to run
