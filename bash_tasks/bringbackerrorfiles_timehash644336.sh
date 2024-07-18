563c6ae5b40eb75c8ae5041f41d3921c80a3d821
Yang Zhou <yangzhou@yangs-mbp-4.wifi.local.cmu.edu>
2024-07-18 11:11:18 -0400

    bringback

---
+++ bash_tasks/bringbackerrorfiles_timehash644336.sh
@@ -0,0 +1,39 @@
+cp /fsx-storygen/beidic/yang/log/log-3393179.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393179.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393178.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393178.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393168.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393168.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393169.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393169.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393170.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393170.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393171.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393171.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393172.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393172.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393173.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393173.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393174.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393174.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393175.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393175.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393176.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393176.out ../submitted
+
+cp /fsx-storygen/beidic/yang/log/log-3393177.err ../submitted
+cp /fsx-storygen/beidic/yang/log/log-3393177.out ../submitted
+
+
+chmod +x ../removetoolarge.sh
+../removetoolarge.sh ../submitted
