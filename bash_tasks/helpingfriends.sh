47c9a226b0ea8dff65116a4f5108c2c01632b9f9
keroro824 <bettychen824@gmail.com>
2024-07-22 21:40:02 +0000

    submit jobs

--- bash_tasks/helpingfriends.sh
+++
@@ -1,2 +0,0 @@
-rm /data/home/beidic/hanshi/Rhodonite-Archive/local/11487.sh
-bash /data/home/beidic/hanshi/Rhodonite-Archive/execute.sh 
--- submitted/helpingfriends.sh
+++ submitted/helpingfriends.sh
@@ -1,9 +1,2 @@
-cd /data/home/beidic/hanshi/Rhodonite-Archive/
-rm .git/index.lock
-echo "Removed .git/index.lock"
-git symbolic-ref HEAD refs/heads/main
-git fetch origin
-git add -A
-git commit -m "fix: resolved issues and added latest files"
-git push origin main
-git status
+rm /data/home/beidic/hanshi/Rhodonite-Archive/local/11487.sh
+bash /data/home/beidic/hanshi/Rhodonite-Archive/execute.sh 
