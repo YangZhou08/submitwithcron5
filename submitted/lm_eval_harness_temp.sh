21336201369ee02c190d23e0aa9ace91a67f6879
keroro824 <bettychen824@gmail.com>
2024-07-30 02:10:02 +0000

    submit jobs

--- submitted/lm_eval_harness_temp.sh
+++ submitted/lm_eval_harness_temp.sh
@@ -39,7 +39,9 @@ cd /fsx-storygen/beidic/yang/GRIFFIN2
 # git checkout exp2 
 # git checkout addinggriffin 
 # git checkout yangexp2two 
-git checkout yangexp2threee 
+git add .
+git stash 
+git checkout yangex3 
 git pull 
 
 git pull 
@@ -69,9 +71,10 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # echo 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 | transformers-cli login 
 huggingface-cli login --token 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 
 
-lengthss=(128 256 512 1024 1500 2048 3072 4096) 
-for length in "${lengthss[@]}" 
-do 
-    python main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=False,check=False,contextlength=$length,kernel_size=10,thr=0.05,attentionimplementation=sdpa --tasks gsm8k_cot --batch_size 1 --limit 0.3 
-    python main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=False,contextlength=$length,kernel_size=10,thr=0.05,attentionimplementation=sdpa --tasks gsm8k_cot --batch_size 1 --limit 0.3 
-done 
+# lengthss=(128 256 512 1024 1500 2048 3072 4096) 
+# for length in "${lengthss[@]}" 
+# do 
+#     python main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=False,check=False,contextlength=$length,kernel_size=10,thr=0.05,attentionimplementation=sdpa --tasks gsm8k_cot --batch_size 1 --limit 0.3 
+#     python main.py --model xhf --model_args pretrained=meta-llama/Meta-Llama-3-8B-Instruct,griffin=True,check=False,contextlength=$length,kernel_size=10,thr=0.05,attentionimplementation=sdpa --tasks gsm8k_cot --batch_size 1 --limit 0.3 
+# done 
+python getcompilego.py 
